0b21c694cfad9c28a688411ce18b2db386501535
IMALIA ROSYIDA <imaliarosyidaa@gmail.com>
2025-08-27 13:23:36 +0700

    add cors enable

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-        origin: ['http://localhost:8081',],
+        origin: ['http://localhost:8081','https://sim-20-sistem-20-integrasi-20-mitra.vercel.app/'],
         credentials: true,
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     });
